5bb37f8344a06e4cd8c9e25ff822de0a02a4dede
us0437 <98657543+us0437@users.noreply.github.com>
2022-07-29 10:37:43 +0530

    Update login.js

--- Moodle+/login.js
+++ Moodle+/login.js
@@ -1,10 +1,43 @@
 console.log("Moodle+ successfully loaded!");
-const login_element = document.querySelector(""); // Fill the selector for the login element in ""
-let login_text = login_element.innerText;
+let login_text = document.getElementById("login").innerText;
 
-let question = ""; // Use split and array operations on the login_text string to extract the question
+let first = login_text.slice(45,69);
+let second = login_text.slice(45,70);
+let add = login_text.slice(45,55);
+let subtract = login_text.slice(45,60);
 
-let answer = ""; // Use if conditions to parse the question and calculate the answer. Make cases for all types of captcha asked
+let answer ; // Use if conditions to parse the question and calculate the answer. Make cases for all types of captcha asked
 
-const captcha_input_element = document.querySelector(""); // Fill the selector for the captcha input element in ""
+if(first == 'Please enter first value'){
+    answer = login_text.slice(70,72);
+}
+
+else if(second == 'Please enter second value'){
+    let matches = login_text.match(/\d+/g);
+    let i = 0;
+              
+    if (matches) {
+        answer = matches[1];
+    }
+}
+
+else if(add == 'Please add'){
+    let matches = login_text.match(/\d+/g);
+    let i = 0;
+
+    if (matches) {
+        answer = (matches[0]*1) + (matches[1]*1);
+    }
+}
+
+else if(subtract == 'Please subtract'){
+    let matches = login_text.match(/\d+/g);
+    let i = 0;
+
+    if (matches) {
+        answer = (matches[0]*1) - (matches[1]*1);
+    }
+}
+
+const captcha_input_element = document.querySelector("#valuepkg3"); // Fill the selector for the captcha input element in ""
 captcha_input_element.value = answer;
