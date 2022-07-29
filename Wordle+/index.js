7679fd017a9bce1a18fb2ca710fe3aa10d67e187
us0437 <98657543+us0437@users.noreply.github.com>
2022-07-29 17:19:18 +0530

    Update index.js

--- Wordle+/index.js
+++ Wordle+/index.js
@@ -1,16 +1,36 @@
-const http = require("http");
-
-const SECRET = "CIGAR"; // You can set any word as the secret answer
-
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 8080;
 function myFunction(req, res) {
-	// console.log({req}); // You can uncomment this to see the request object
-	console.log(req.url);
+    let query = req.url.split("q=")[1];
+    let output = "";
+    let word = ['C','I','G','A','R'];
+    if (query == null) {
+        output = "undefined";
+    }
+    else {
+        let i = 0;
+        while(i<5){
+            if(query[i]==word[i]){
+                output+='g';
+            }
+            else if(query[i]=='C' || query[i]=='I' || query[i]=='G' || query[i]=='A' || query[i]=='R'){
+                output+='y';
+            }
+            else{
+                output+='r';
+            }
+            i++;
+        }
+    }
+    console.log(output)
+    res.statusCode = 200;
+    // res.setHeader('Content-Type', 'text/plain');
+    res.end(output)
 
-	const GUESS = ""; // Write logic to parse the word which the user guessed from the URL string
-	const feedback = ""; // Write logic to compare the word with the secret, and generate the feedback string
-
-	res.write(feedback);
-	res.end();
 }
-
-http.createServer(myFunction).listen(8080);
+const server = http.createServer(myFunction);
+function callBack() {
+    console.log('Server running at  http://${hostname}:${port}/');
+}
+server.listen(port, hostname, callBack);
