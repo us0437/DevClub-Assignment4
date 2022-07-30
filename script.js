0a98a36368269983d7dad562212260d014ed9ae1
us0437 <98657543+us0437@users.noreply.github.com>
2022-07-30 19:19:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
-console.log("Loaded script.js");
+$(document).ready(function(){
+    var url="https://data.covid19india.org/data.json"
+
+    $.getJSON(url,function(data){
+        console.log(data)
+
+        var total_recovered,total_deaths,total_confirmed
+        var date1=[]
+        var confirmed1=[]
+        var recovered1=[]
+        var deaths1=[]
+        var date=[]
+        var confirmed=[]
+        var recovered=[]
+        var deaths=[]
+        $.each(data.cases_time_series,function(id,obj){
+            date1.push(obj.dateymd)
+            confirmed1.push(obj.totalconfirmed)
+            recovered1.push(obj.totalrecovered)
+            deaths1.push(obj.totaldeceased)
+            date =date1.slice(534)
+            confirmed=confirmed1.slice(534)
+            deaths=deaths1.slice(534)
+            recovered=recovered1.slice(534)
+
+        })
+        console.log(date)
+        var mychart=document.getElementById("chart").getContext('2d')
+        var chart=new Chart(mychart,{
+            type:'line',
+            data:{
+                labels:date,
+                datasets:[
+                    {
+                        label:"Recovered Cases",
+                        data: recovered,
+                        backgroundColor:"#2ecc71",
+                        minBarLength:100
+                    },
+                    {
+                        label:"Confirmed Cases",
+                        data: confirmed,
+                        backgroundColor:"#f1c40f",
+                        minBarLength:100
+                    },
+                    {
+                        label:"Deaths",
+                        data: deaths,
+                        backgroundColor:"#ff0000",
+                        minBarLength:100
+                    },
+                ]
+            },
+            options:{}
+
+        })
+        
+    })
+})
